71d40485c4633c1ae062a94f3bf088c4c303cfbe
jack chou <jack.chou@quanta.corp-partner.google.com>
2018-10-09 14:42:27 +0800

    load usb path, vendor and size

--- usb-format.sh
+++ usb-format.sh
@@ -1,5 +1,21 @@
-#/bin/bash
+#!/bin/bash
 
-usb_list=$(lsblk -p -S -o NAME,MODEL,SIZE,VENDOR,TRAN | grep 'usb'| sed -r 's/usb/xxxx/g')
+name_list=$(lsblk -p -S -o NAME,TRAN | grep 'usb' | sed -e 's/usb//g' )
+vendor_list=$(lsblk -p -S -o VENDOR,TRAN | grep 'usb' | sed -e 's/usb//g')
+size_list=$(lsblk -p -S -o SIZE,TRAN | grep 'usb' | sed -e 's/usb//g' )
 
-echo $usb_list
+for ((i=0; i<${#name_list[@]}; i++));do
+    echo ${name_list[$i]}
+    echo ${vendor_list[$i]}
+    echo ${size_list[$i]}
+done
+#echo ${usb_name_list[0]}
+#echo ${usb_name_list[1]}
+
+#if read -p "please selesct your usb which need format [0-${#usb_list}]: " format_usb
+#then
+#    echo $format_usb
+#else
+#    echo "please select usb device."
+#    exit 0
+#fi
